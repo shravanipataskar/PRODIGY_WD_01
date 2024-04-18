b155b8f1a289d168287f4d5fa9c52e5451825b2b
shravanipataskar <167396123+shravanipataskar@users.noreply.github.com>
2024-04-18 10:10:37 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,12 @@
+function mouseOver(element)
+{
+    element.style.color="blue";
+}
+function mouseOut(element) 
+{
+    element.style.color="white";
+}
+function mouseOut1(element) 
+{
+    element.style.color="black";
+}
